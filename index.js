e5a064be7595034ffcea1b8e37ad7d79460e145e
Cam <camerontullos@macbook-air.attlocal.net>
2017-04-30 20:23:59 -0400

    Cleaned up code and formatting.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * @description Fabricator module that creates a dependency tracker.
  * @author Cam Tullos cam@tullos.ninja
@@ -6,245 +8,265 @@
 /**
  * Required dependencies
  */
-const globby = require('globby');
-const fs = require('fs');
-const path = require('path');
-const _ = require('lodash');
-const matter = require('gray-matter');
-const yaml = require('js-yaml');
-const beautify_js = require('js-beautify');
+const globby         = require('globby');
+const fs             = require('fs');
+const path           = require('path');
+const _              = require('lodash');
+const matter         = require('gray-matter');
+const yaml           = require('js-yaml');
+const beautify_js    = require('js-beautify');
 
 
 const dna = {
-	dependencies: function(key, helix, block) {
-		return dna.get(key, helix, 'dependencies', block);
-	},
+    dependencies: function (key, helix, block) {
+        return dna.get(key, helix, 'dependencies', block);
+    },
 
-	dependents: function(key, helix, block) {
-		return dna.get(key, helix, 'dependents', block);
-	},
+    dependents: function (key, helix, block) {
+        return dna.get(key, helix, 'dependents', block);
+    },
 
-	get: function (key, helix, field, block) {
-		block = block || {fn: function () { return arguments[arguments.length - 1]; }};
+    get: function (key, helix, field, block) {
+        let output    = '';
+        key           = (typeof key === 'string') ? key : key[0];
+        block         = block || {
+            fn: function () {
+                return arguments[arguments.length - 1];
+            }
+        };
 
-		key = (typeof key === 'string') ? key : key[0];
+        for (let prop in helix) {
+            if (helix[prop]['id'] === key) {
+                let deps    = helix[prop][field];
+                let i       = -1;
 
-		var output = '';
+                for (let p in deps) {
+                    let data = {};
+                    i++;
 
-		for (let prop in helix) {
-			if (helix[prop]['id'] === key) {
-				var deps = helix[prop][field];
+                    deps[p].sort();
 
-				var i = -1;
-				for (let p in deps) {
-					let data = {};
-					i++;
+                    data['count']        = deps[p].length;
+                    data['selectors']    = deps[p];
+                    data['link']         = dna.link(p);
+                    data['file']         = p;
+                    data['index']        = i;
 
-					deps[p].sort();
-					data['count'] = deps[p].length;
-					data['selectors'] = deps[p];
-					data['link'] = dna.link(p);
-					data['file'] = p;
-					data['index'] = i;
+                    output += block.fn(data);
+                }
+            }
+        }
 
-					output += block.fn(data);
-				}
-			}
-		}
+        return output;
+    },
 
-		return output;
-	},
+    getName: function (filePath, preserveNumbers) {
+        // get name; replace spaces with dashes
+        let name = path.basename(filePath, path.extname(filePath)).replace(/\s/g, '-');
+        return (preserveNumbers) ? name : name.replace(/^[0-9|\.\-]+/, '');
+    },
 
-	getName: function (filePath, preserveNumbers) {
-		// get name; replace spaces with dashes
-		var name = path.basename(filePath, path.extname(filePath)).replace(/\s/g, '-');
-		return (preserveNumbers) ? name : name.replace(/^[0-9|\.\-]+/, '');
-	},
+    getMatter: function (file) {
+        return matter.read(file, {
+            parser: require('js-yaml').safeLoad
+        });
+    },
 
-	getMatter: function (file) {
-		return matter.read(file, {
-			parser: require('js-yaml').safeLoad
-		});
-	},
+    check: function (key, helix, field) {
+        key = (typeof key === 'string') ? key : key[0];
 
-	check: function (key, helix, field) {
-		key = (typeof key === 'string') ? key : key[0];
+        for (let prop in helix) {
+            if (helix[prop]['id'] === key) {
+                let deps = helix[prop][field];
+                for (let p in deps) {
+                    if (deps[p].length > 0) {
+                        return true;
+                    }
+                }
+            }
+        }
 
-		for (let prop in helix) {
-			if (helix[prop]['id'] === key) {
-				var deps = helix[prop][field];
-				for (let p in deps) {
-					if (deps[p].length > 0) { return true; }
-				}
-			}
-		}
+        return false;
+    },
 
-		return false;
-	},
+    hasDependencies: function (key, helix, options) {
+        options = options || {
+                fn: function () {
+                    return arguments[arguments.length - 1];
+                }
+            };
+        let status = dna.check(key, helix, 'dependencies');
+        if (status === true) {
+            return options.fn(this);
+        } else {
+            return options.inverse(this);
+        }
+    },
 
-	hasDependencies: function (key, helix, options) {
-		options = options || {fn: function () { return arguments[arguments.length - 1]; }};
-		var status = dna.check(key, helix, 'dependencies');
-		if (status === true) {
-			return options.fn(this);
-		} else {
-			return options.inverse(this);
-		}
-	},
+    hasDependents: function (key, helix, options) {
+        options = options || {
+                fn: function () {
+                    return arguments[arguments.length - 1];
+                }
+            };
+        let status = dna.check(key, helix, 'dependents');
+        if (status === true) {
+            return options.fn(this);
+        } else {
+            return options.inverse(this);
+        }
+    },
 
-	hasDependents: function (key, helix, options) {
-		options = options || {fn: function () { return arguments[arguments.length - 1]; }};
-		var status = dna.check(key, helix, 'dependents');
-		if (status === true) {
-			return options.fn(this);
-		} else {
-			return options.inverse(this);
-		}
-	},
+    link: function (str) {
 
-	link: function (str) {
+        let a = str.split('.');
+        let e = a.pop();
+        let l;
 
-		var a = str.split('.');
-		var e = a.pop();
-		var l;
+        a = a.join('.').split('/');
 
-		a = a.join('.').split('/');
+        l = [a[2], '.', e];
 
-		l = [a[2], '.', e];
+        if (a[3]) {
+            l.push('#', a[3]);
+        }
 
-		if (a[3]) {
-		  l.push('#', a[3]);
-		}
+        if (a[4]) {
+            l.push('.', a[4]);
+        }
 
-		if (a[4]) {
-		  l.push('.', a[4]);
-		}
+        l = l.join('');
 
-		l = l.join('');
+        return l;
+    },
 
-		return l;
-	},
+    scan: function (config) {
+        let defaults = {
+            dna          : 'src/data/dependencies.json',
+            materials    : ['src/materials/**/*'],
+            views        : ['src/views/**/*', '!src/views/+(layouts)/**']
+        };
 
-	scan: function (config) {
-		var defaults = {
-			dna: 'src/data/dependencies.json',
-			materials: ['src/materials/**/*'],
-			views: ['src/views/**/*', '!src/views/+(layouts)/**']
-		};
+        config = config || {};
 
-		config = config || {};
+        for (let prop in defaults) {
+            if (!config.hasOwnProperty(prop)) {
+                config[prop] = defaults[prop];
+            }
+        }
 
-		for (let prop in defaults) {
-			if (!config.hasOwnProperty(prop)) {
-				config[prop] = defaults[prop];
-			}
-		}
+        let files    = [];
+        let res      = {helix: {}};
 
-		var files = [];
-		var res = {helix: {}};
+        let dirs = [
+            config.materials,
+            config.views
+        ];
 
-		var dirs = [
-			config.materials,
-			config.views
-		];
-
-		// Get partials (HTML files)
-		dirs.forEach(function (dir) {
-			var read = globby.sync(dir, {nodir: true});
-			files = files.concat(read);
-		});
+        // Get partials (HTML files)
+        dirs.forEach(function (dir) {
+            let read    = globby.sync(dir, {nodir: true});
+            files       = files.concat(read);
+        });
 
 
-		files.forEach(function (file) {
-			// Get dna
-			var fm = dna.getMatter(file);
+        files.forEach(function (file) {
+            // Get dna
+            let fm             = dna.getMatter(file);
+            res.helix[file]    = {dna: [], id: null, dependents: {}, dependencies: {}};
 
-			res.helix[file] = {dna: [], id: null, dependents: {}, dependencies: {}};
-			if (fm.data.dna) {
-				res.helix[file]['dna'] = (typeof fm.data.dna === 'string') ? [fm.data.dna] : fm.data.dna;
-				res.helix[file]['id'] = _.head(res.helix[file]['dna']);
-			}
+            if (fm.data.dna) {
+                res.helix[file]['dna']    = (typeof fm.data.dna === 'string') ? [fm.data.dna] : fm.data.dna;
+                res.helix[file]['id']     = _.head(res.helix[file]['dna']);
+            }
 
-			var props = res.helix[file]['dna'];
+            let props = res.helix[file]['dna'];
 
-			// Get dependents
-			var others = _.without(files, file);
-			others.forEach(function (ofile) {
-				let content = String(fs.readFileSync(ofile, 'utf-8'));
-				let elements = [];
-				let findTag = /<[^\/].*?>/g;
-				let element = findTag.exec(content);
-				let matches = [];
+            // Get dependents
+            let others = _.without(files, file);
+            others.forEach(function (ofile) {
+                let content     = String(fs.readFileSync(ofile, 'utf-8'));
+                let elements    = [];
+                let findTag     = /<[^\/].*?>/g;
+                let element     = findTag.exec(content);
+                let matches     = [];
 
-				while (element) {
-					element = element[0];
+                while (element) {
+                    element = element[0];
 
-					// Match id attribute
-					let id = (element.match(/id s=["|'](.*?)["|']/i) || [, ""])[1];
-					if (id) {
-						let p = '#';
-						let i = _.intersection([id], props);
-						i.forEach(function (m) { matches.push(p + m); });
-					}
+                    // Match id attribute
+                    let id = (element.match(/id s=["|'](.*?)["|']/i) || [, ""])[1];
+                    if (id) {
+                        let p    = '#';
+                        let i    = _.intersection([id], props);
 
-					// Match data-dna attribute
-					let cmp = (element.match(/data-dna=["|'](.*?)["|']/i) || [, ""])[1];
-					if (cmp) {
-						let p = 'data-dna=';
-						let i = _.intersection([cmp], props);
-						i.forEach(function (m) { matches.push(p + m); });
-					}
+                        i.forEach(function (m) {
+                            matches.push(p + m);
+                        });
+                    }
 
-					// Match class attribute
-					let classes = (element.match(/class=["|'](.*?)["|']/i) || [,""])[1].split(' ');
-					classes = _.compact(classes);
-					if (classes.length > 0) {
-						let p = '.';
-						let i = _.intersection(_.compact(classes), props);
-						i.forEach(function (m) { matches.push(p + m); });
-					}
+                    // Match data-dna attribute
+                    let cmp = (element.match(/data-dna=["|'](.*?)["|']/i) || [, ""])[1];
+                    if (cmp) {
+                        let p    = 'data-dna=';
+                        let i    = _.intersection([cmp], props);
 
-					element = findTag.exec(content);
-				}
+                        i.forEach(function (m) {
+                            matches.push(p + m);
+                        });
+                    }
 
-				matches = _.uniq(matches);
+                    // Match class attribute
+                    let classes = (element.match(/class=["|'](.*?)["|']/i) || [, ""])[1].split(' ');
+                    classes = _.compact(classes);
+                    if (classes.length > 0) {
+                        let p    = '.';
+                        let i    = _.intersection(_.compact(classes), props);
 
-				if (matches.length > 0) {
-					res.helix[file]['dependents'][ofile] = matches;
-				}
-			});
-		});
+                        i.forEach(function (m) {
+                            matches.push(p + m);
+                        });
+                    }
 
-		files.forEach(function (file) {
-		  var others = _.without(files, file);
-		  others.forEach(function (ofile) {
-			let d = res.helix[ofile]['dependents'][file];
-			if (d) {
-			  res.helix[file]['dependencies'][ofile] = d;
-			}
-		  });
-		});
+                    element = findTag.exec(content);
+                }
 
-		var output = JSON.stringify(res);
-			output = beautify_js(output, {indent_size:2});
+                matches = _.uniq(matches);
 
-		fs.writeFileSync(config.dna, output);
-	}
+                if (matches.length > 0) {
+                    res.helix[file]['dependents'][ofile] = matches;
+                }
+            });
+        });
+
+        files.forEach(function (file) {
+            let others = _.without(files, file);
+            others.forEach(function (ofile) {
+                let d = res.helix[ofile]['dependents'][file];
+                if (d) {
+                    res.helix[file]['dependencies'][ofile] = d;
+                }
+            });
+        });
+
+        let output    = JSON.stringify(res);
+        output        = beautify_js(output, {indent_size: 2});
+
+        fs.writeFileSync(config.dna, output);
+    }
 
 };
-
-
 
 
 /**
  * Exports
  */
 module.exports = {
-	check: dna.check,
-	dependencies: dna.dependencies,
-	dependents: dna.dependents,
-	hasDependencies: dna.hasDependencies,
-	hasDependents: dna.hasDependents,
-	link: dna.link,
-	scan: dna.scan
-}
+    check              : dna.check,
+    dependencies       : dna.dependencies,
+    dependents         : dna.dependents,
+    hasDependencies    : dna.hasDependencies,
+    hasDependents      : dna.hasDependents,
+    link               : dna.link,
+    scan               : dna.scan
+};
--- test/index.js
+++ test/index.js
@@ -1,23 +1,25 @@
-var should 	= require('chai').should(),
-    dna 	= require('../index.js'),
-	data 	= require('./data.json');
+'use strict';
+
+const should    = require('chai').should(),
+    dna       = require('../index.js'),
+    data      = require('./data.json');
 
 
-describe('#check', function() {
+describe('#check', function () {
 
-	it('check(btn-group, dependents -> true)', function () {
-		dna.check('btn-group', data.helix, 'dependents').should.equal(true);
-	});
+    it('check(btn-group, dependents -> true)', function () {
+        dna.check('btn-group', data.helix, 'dependents').should.equal(true);
+    });
 
-	it('check(btn-group, dependencies -> true)', function () {
-		dna.check('btn-group', data.helix, 'dependencies').should.equal(true);
-	});
+    it('check(btn-group, dependencies -> true)', function () {
+        dna.check('btn-group', data.helix, 'dependencies').should.equal(true);
+    });
 
-	it('check(btn, dependencies -> false)', function () {
-		dna.check('btn', data.helix, 'dependencies').should.equal(false);
-	});
+    it('check(btn, dependencies -> false)', function () {
+        dna.check('btn', data.helix, 'dependencies').should.equal(false);
+    });
 
-	it('check(btn-active, dependents -> true)', function () {
-		dna.check('btn-active', data.helix, 'dependents').should.equal(false);
-	});
+    it('check(btn-active, dependents -> true)', function () {
+        dna.check('btn-active', data.helix, 'dependents').should.equal(false);
+    });
 });
